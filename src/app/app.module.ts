3af73122c841826f5b19bca3898ef51ad711aad1
Paolo Monella <paolo.monella@gmx.net>
2019-10-18 19:00:07 +0200

    Add HelpPanelComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HelpPanelComponent } from './help-panel/help-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelpPanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'help', component: HelpPanelComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
