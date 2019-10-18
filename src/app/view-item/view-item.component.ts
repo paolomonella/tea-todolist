1e01519c143d8457a07fb5a6c62f91bcf35b7350
Giacomo Cerrretini <giacomocerrretini@MacBook-Air-di-Giacomo.local>
2019-10-18 15:39:05 +0200

    Add todoItem iterface

--- src/app/view-item/view-item.component.ts
+++ src/app/view-item/view-item.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { stringify } from '@angular/compiler/src/util';
+import { Component } from '@angular/core';
+
+interface itemTodo{
+  list:string;
+  id:number;
+  title:string;
+  description?: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'view-item',
   templateUrl: './view-item.component.html',
   styleUrls: ['./view-item.component.scss']
 })
-export class ViewItemComponent implements OnInit {
-  items: object[]
-  constructor() { }
 
-  ngOnInit() {
-    this.items = [
-      {
-        list: 'example',
-        id: 1,
-        title: 'Example title',
-        description: 'a item in example list for the view model',
-        completed: false
-      }
-    ]
+export class ViewItemComponent {
+
+  items: itemTodo = {
+    list: 'example',
+    id: 1,
+    title: 'Example title',
+    description: 'a item in example list for the view model',
+    completed: false,
+  }
+
+  constructor(
+  ){
+
   }
 
 }
